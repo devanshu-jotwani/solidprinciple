8ce39c37c5904c1fc2acd6952382f20372661226
devanshu-jotwani <devanshujotwani14@gmail.com>
2021-12-24 16:04:47 +0530

    interface segregation principle

---
+++ isp.py
@@ -0,0 +1,21 @@
+class Movable(ABC):
+    @abstractmethod
+    def go(self):
+        pass
+
+
+class Flyable(Movable):
+    @abstractmethod
+    def fly(self):
+        pass
+
+class Aircraft(Flyable):
+    def go(self):
+        print("Taxiing")
+
+    def fly(self):
+        print("Flying")
+
+class Car(Movable):
+    def go(self):
+        print("Going")
